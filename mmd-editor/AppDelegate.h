709d0ebbab2322b25157d29dab39ac57e1989a4a
Fletcher T. Penney <fletcher@fletcherpenney.net>
2011-07-10 17:28:22 -0400

    improve prefs for styles

---
+++ mmd-editor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  mmd-editor
+//
+//  Created by Fletcher T. Penney on 7/10/11.
+//  Copyright 2011 Fletcher T. Penney. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface AppDelegate : NSObject {
+    IBOutlet NSPopUpButton *stylesChooser;
+
+}
+
+@end
---
+++ mmd-editor/AppDelegate.m
@@ -0,0 +1,32 @@
+//
+//  AppDelegate.m
+//  mmd-editor
+//
+//  Created by Fletcher T. Penney on 7/10/11.
+//  Copyright 2011 Fletcher T. Penney. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+
+@implementation AppDelegate
+
+- (void) populateStylesPopUpButton
+{
+	[stylesChooser removeAllItems];
+	[stylesChooser addItemWithTitle:@"Default"];	
+
+	NSArray *styleFiles = [[NSBundle mainBundle] pathsForResourcesOfType:@"style"
+															 inDirectory:nil];
+	for (NSString *file in styleFiles)
+		[stylesChooser addItemWithTitle:[[file lastPathComponent] stringByDeletingPathExtension]];
+	
+	[stylesChooser selectItemWithTitle:[[NSUserDefaults standardUserDefaults] stringForKey:@"defaultStyleSheet"]];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	[self populateStylesPopUpButton];
+}
+
+@end
